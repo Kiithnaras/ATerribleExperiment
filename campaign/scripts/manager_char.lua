854a5a268130382c9d577b35c84c7e9dd6c1dd35
Kiithnaras <kiithnaras@gmail.com>
2015-03-09 14:52:25 -0400

    Skill rank behavior changes

--- campaign/scripts/manager_char.lua
+++ campaign/scripts/manager_char.lua
@@ -726,11 +726,7 @@ function updateSkillPoints(nodeChar)
 	local nSpent;
 	for _,vNode in pairs(DB.getChildren(nodeChar, "skilllist")) do
 		nSpent = DB.getValue(vNode, "ranks", 0);
-		if nSpent > 0 then
-			if not bPFMode and DB.getValue(vNode, "state", 0) == 0 then
-				nSpent = nSpent * 2;
-			end
-			
+		if nSpent > 0 then			
 			nSpentTotal = nSpentTotal + nSpent;
 		end
 	end
